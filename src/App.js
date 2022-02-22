7f1b9d763d605f367f5ef7e73c6349cd1a7fe932
blue-cyborg <87626094+blue-cyborg@users.noreply.github.com>
2022-02-21 22:41:25 -0500

    Tinkering with css
    playing with the css as I want to make it appear different than my first weather app

--- src/App.js
+++ src/App.js
@@ -6,10 +6,12 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>
-          Weather App
+          Weather Forecast
         </h1>
+        <h2>Find the forecast in your city</h2>
         <SearchEngine />
       </header>
+      <p>Open source code coded by Candice Drave</p>
     </div>
   );
 }
